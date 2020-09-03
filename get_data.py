92946928cfec2777ac0b938c72709692497cef35
Yuxin <yuxinsun@hotmail.com>
2020-09-03 23:23:13 +0100

    ~

--- get_data.py
+++ get_data.py
@@ -1,6 +1,7 @@
 #------ IMPORT LIBRARY and CONFIG -------#
 import alpaca_trade_api as tradeapi
 import Yuxin_config
+import config
 import pandas as pd
 pd.options.display.max_rows = 999
 pd.set_option('display.max_columns', None)
